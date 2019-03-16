25e0f5f424637dfd3fe7db36a8a1a30792cbc14a
roman klawinski <jrklawin@svsu.edu>
2019-03-16 13:31:28 -0400

    a

--- fr_per_create2.php
+++ fr_per_create2.php
@@ -18,38 +18,22 @@ session_start();
 require "../PhpProject/database.php";
 if ( !empty($_POST)) { // if not first time through
 	// initialize user input validation variables
-	$fnameError = null;
-	$lnameError = null;
+	$nameError = null;
 	$emailError = null;
 	$mobileError = null;
 	$passwordError = null;
-	$titleError = null;
-	$pictureError = null; // not used
 	
 	// initialize $_POST variables
-	$fname = $_POST['fname'];
-	$lname = $_POST['lname'];
+	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$mobile = $_POST['mobile'];
 	$password = $_POST['password'];
 	$passwordhash = MD5($password);
-	$title =  $_POST['title'];
-	$picture = $_POST['picture']; // not used
 	
-	// initialize $_FILES variables
-	$fileName = $_FILES['userfile']['name'];
-	$tmpName  = $_FILES['userfile']['tmp_name'];
-	$fileSize = $_FILES['userfile']['size'];
-	$fileType = $_FILES['userfile']['type'];
-	$content = file_get_contents($tmpName);
 	// validate user input
 	$valid = true;
 	if (empty($fname)) {
 		$fnameError = 'Please enter First Name';
-		$valid = false;
-	}
-	if (empty($lname)) {
-		$lnameError = 'Please enter Last Name';
 		$valid = false;
 	}
 	// do not allow 2 records with same email address!
@@ -60,15 +44,6 @@ if ( !empty($_POST)) { // if not first time through
 		$emailError = 'Please enter a valid Email Address';
 		$valid = false;
 	}
-	$pdo = Database::connect();
-	$sql = "SELECT * FROM fr_persons";
-	foreach($pdo->query($sql) as $row) {
-		if($email == $row['email']) {
-			$emailError = 'Email has already been registered!';
-			$valid = false;
-		}
-	}
-	Database::disconnect();
 	
 	// email must contain only lower case letters
 	if (strcmp(strtolower($email),$email)!=0) {
@@ -80,32 +55,9 @@ if ( !empty($_POST)) { // if not first time through
 		$mobileError = 'Please enter Mobile Number (or "none")';
 		$valid = false;
 	}
-	if(!preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $mobile)) {
-		$mobileError = 'Please write Mobile Number in form 000-000-0000';
-		$valid = false;
-	}
 	if (empty($password)) {
 		$passwordError = 'Please enter valid Password';
 		$valid = false;
-	}
-	if (empty($title)) {
-		$titleError = 'Please enter valid Title';
-		$valid = false;
-	}
-	// restrict file types for upload
-	$types = array('image/jpeg','image/gif','image/png');
-	if($filesize > 0) {
-		if(in_array($_FILES['userfile']['type'], $types)) {
-		}
-		else {
-			$filename = null;
-			$filetype = null;
-			$filesize = null;
-			$filecontent = null;
-			$pictureError = 'improper file type';
-			$valid=false;
-			
-		}
 	}
 	// insert data
 	if ($valid) 
@@ -113,20 +65,15 @@ if ( !empty($_POST)) { // if not first time through
 		$pdo = Database::connect();
 		
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO fr_persons (fname,lname,email,mobile,password,title,
-		filename,filesize,filetype,filecontent) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		$sql = "INSERT INTO fr_persons (name,email,mobile,passwordhash) values(?, ?, ?, ?)";
 		$q = $pdo->prepare($sql);
-		$q->execute(array($fname,$lname,$email,$mobile,$passwordhash,$title,
-		$fileName,$fileSize,$fileType,$content));
+		$q->execute(array($name,$email,$mobile,$passwordhash));
 		
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT * FROM fr_persons WHERE email = ? AND password = ? LIMIT 1";
+		$sql = "SELECT * FROM customers WHERE email = ? AND password = ? LIMIT 1";
 		$q = $pdo->prepare($sql);
 		$q->execute(array($email,$passwordhash));
 		$data = $q->fetch(PDO::FETCH_ASSOC);
-		
-		$_SESSION['fr_person_id'] = $data['id'];
-		$_SESSION['fr_person_title'] = $data['title'];
 		
 		Database::disconnect();
 		header("Location: fr_events.php");
@@ -152,30 +99,21 @@ if ( !empty($_POST)) { // if not first time through
 				Functions::logoDisplay2();
 			?>
 			<div class="row">
-				<h3>Add New Volunteer</h3>
+				<h3>Add New</h3>
 			</div>
 	
 			<form class="form-horizontal" action="fr_per_create2.php" method="post" enctype="multipart/form-data">
 
-				<div class="control-group <?php echo !empty($fnameError)?'error':'';?>">
+				<div class="control-group <?php echo !empty(Error)?'error':$nameError'';?>">
 					<label class="control-label">First Name</label>
 					<div class="controls">
-						<input name="fname" type="text"  placeholder="First Name" value="<?php echo !empty($fname)?$fname:'';?>">
+						<input name="name" type="text"  placeholder="First Name" value="<?php echo !empty($fname)?$fname:'';?>">
 						<?php if (!empty($fnameError)): ?>
 							<span class="help-inline"><?php echo $fnameError;?></span>
 						<?php endif; ?>
 					</div>
 				</div>
 				
-				<div class="control-group <?php echo !empty($lnameError)?'error':'';?>">
-					<label class="control-label">Last Name</label>
-					<div class="controls">
-						<input name="lname" type="text"  placeholder="Last Name" value="<?php echo !empty($lname)?$lname:'';?>">
-						<?php if (!empty($lnameError)): ?>
-							<span class="help-inline"><?php echo $lnameError;?></span>
-						<?php endif; ?>
-					</div>
-				</div>
 				
 				<div class="control-group <?php echo !empty($emailError)?'error':'';?>">
 					<label class="control-label">Email</label>
@@ -206,29 +144,9 @@ if ( !empty($_POST)) { // if not first time through
 						<?php endif;?>
 					</div>
 				</div>
-				
-				<div class="control-group">
-					<label class="control-label">Title</label>
-					<div class="controls">
-						<select class="form-control" name="title">
-							<option value="Volunteer" selected>Volunteer</option>
-							<!-- <option value="Administrator" >Administrator</option> -->
-						</select>
-					</div>
-				</div>
-			  
-				<div class="control-group <?php echo !empty($pictureError)?'error':'';?>">
-					<label class="control-label">Picture</label>
-					<div class="controls">
-						<input type="hidden" name="MAX_FILE_SIZE" value="16000000">
-						<input name="userfile" type="file" id="userfile">
-						
-					</div>
-				</div>
 			  
 				<div class="form-actions">
 					<button type="submit" class="btn btn-success">Confirm</button>
-					<a class="btn" href="fr_events.php">Back</a>
 				</div>
 				
 			</form>
