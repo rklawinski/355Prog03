356b8a80ebd667f434be1f258561e8972b108e59
roman klawinski <jrklawin@svsu.edu>
2019-03-16 11:18:03 -0400

    first

---
+++ login.php
@@ -0,0 +1,113 @@
+<?php
+/* ---------------------------------------------------------------------------
+ * filename    : login.php
+ * author      : George Corser, gcorser@gmail.com
+ * description : This program logs the user in by setting $_SESSION variables
+ * ---------------------------------------------------------------------------
+ */
+// Start or resume session, and create: $_SESSION[] array
+session_start(); 
+// include the class that handles database connections
+require "../../database/database.php";
+
+if ( !empty($_POST)) { // if $_POST filled then process the form
+	// initialize $_POST variables
+	$username = $_POST['username']; // username is email address
+	$password = $_POST['password'];
+	$passwordhash = MD5($password);
+	$labelError = "";
+		
+	// verify the username/password
+	$pdo = Database::connect();
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "SELECT * FROM customers WHERE email = ? AND password_hash = ? LIMIT 1";
+	$q = $pdo->prepare($sql);
+	$q->execute(array($username,$passwordhash));
+	$data = $q->fetch(PDO::FETCH_ASSOC);
+	
+	if($data) { // if successful login set session variables
+		// echo "success!";
+		$_SESSION['tJHSQRuoNnWUwLRe'] = $data['id'];
+		$_SESSION['name'] = $data['name'];
+		$_SESSION['email'] = $data['email'];
+		$_SESSION['mobile'] = $data['mobile'];
+		$sessionid = $data['id'];
+		Database::disconnect();
+		header("Location: customers.php ");
+		// javascript below is necessary for system to work on github
+		// echo "<script type='text/javascript'> document.location = 'fr_assignments.php'; </script>";
+		exit();
+	}
+	else { // display error message
+		Database::disconnect();
+		$labelError = "Incorrect username/password";
+	}
+
+
+} 
+// if $_POST NOT filled then display login form, below.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta charset='UTF-8'>
+        <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+	</head>
+
+<body>
+    <div class="container">
+		<div class="span10 offset1">
+
+			<div class="row">
+				<h3>Login</h3>
+			</div>
+
+			<form class="form-horizontal" action="login.php" method="post">
+								  
+				<div class="control-group">
+					<label class="control-label">Username (Email)</label>
+					<div class="controls">
+						<input name="username" type="text"  placeholder="me@email.com" required> 
+					</div>	
+				</div> 
+				
+				<div class="control-group">
+					<label class="control-label">Password</label>
+					<div class="controls">
+						<input name="password" type="password" placeholder="not your SVSU password, please" required> 
+					</div>	
+				</div> 
+
+				<div class="form-actions">
+					<button type="submit" class="btn btn-success">Sign in</button>
+					&nbsp; &nbsp;
+					<a class="btn btn-primary" href="fr_per_create2.php">Join (New Volunteer)</a>
+				</div>
+				
+				<div>
+					<?php
+						echo "<br>";
+						echo "<span style='color: red;' class='help-inline'>";
+						echo "&nbsp;&nbsp;" . $labelError;
+						echo "</span>";
+						echo "<br>";
+					?>
+				</div>
+				
+				<footer>
+					<small>&copy; Copyright 2019, George Corser
+					</small>
+				</footer>
+				
+			</form>
+
+
+		</div> <!-- end div: class="span10 offset1" -->
+				
+    </div> <!-- end div: class="container" -->
+
+  </body>
+  
+</html>
