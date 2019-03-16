bcc76161c62eb23f2870876309e99f94f52a1e56
roman klawinski <jrklawin@svsu.edu>
2019-03-16 13:57:46 -0400

    a

--- fr_per_create2.php
+++ fr_per_create2.php
@@ -1,150 +1,103 @@
-
-243 lines (212 sloc) 7.43 KB
-<?php 
+<?php
 /* ---------------------------------------------------------------------------
- * filename    : fr_per_create2.php
+ * filename    : login.php
  * author      : George Corser, gcorser@gmail.com
- * description : This program adds/inserts a new volunteer (table: fr_persons)
+ * description : This program logs the user in by setting $_SESSION variables
  * ---------------------------------------------------------------------------
  */
-session_start();
-// session_start();
-// if(!isset($_SESSION["fr_person_id"])){ // if "user" not set,
-	// session_destroy();
-	// header('Location: login.php');     // go to login page
-	// exit;
-// }
-	
+// Start or resume session, and create: $_SESSION[] array
+session_start(); 
+// include the class that handles database connections
 require "../PhpProject/database.php";
-	$nameError = null;
-	$emailError = null;
-	$mobileError = null;
-	$passwordError = null;
-	
-	// initialize $_POST variables
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$mobile = $_POST['mobile'];
+
+if ( !empty($_POST)) { // if $_POST filled then process the form
+    // initialize $_POST variables
+    $username = $_POST['name']; // username is email address
+    $username = $_POST['username']; // username is email address
+    $username = $_POST['mobile']; // username is email address
 	$password = $_POST['password'];
 	$passwordhash = MD5($password);
-	
-	// validate user input
-	$valid = true;
-	if (empty($fname)) {
-		$fnameError = 'Please enter First Name';
-		$valid = false;
-	}
-	// do not allow 2 records with same email address!
-	if (empty($email)) {
-		$emailError = 'Please enter valid Email Address (REQUIRED)';
-		$valid = false;
-	} else if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
-		$emailError = 'Please enter a valid Email Address';
-		$valid = false;
-	}
-	
-	// email must contain only lower case letters
-	if (strcmp(strtolower($email),$email)!=0) {
-		$emailError = 'email address can contain only lower case letters';
-		$valid = false;
-	}
-	
-	if (empty($mobile)) {
-		$mobileError = 'Please enter Mobile Number (or "none")';
-		$valid = false;
-	}
-	if (empty($password)) {
-		$passwordError = 'Please enter valid Password';
-		$valid = false;
-	}
-	// insert data
-	if ($valid) 
-	{
-		$pdo = Database::connect();
+	$labelError = "";
 		
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "INSERT INTO customers (name,email,mobile,passwordhash) values(?, ?, ?, ?)";
-		$q = $pdo->prepare($sql);
-		$q->execute(array($name,$email,$mobile,$passwordhash));
-		
-		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$sql = "SELECT * FROM customers WHERE email = ? AND password = ? LIMIT 1";
-		$q = $pdo->prepare($sql);
-		$q->execute(array($email,$passwordhash));
-		$data = $q->fetch(PDO::FETCH_ASSOC);
-		
-		Database::disconnect();
-		header("Location: fr_events.php");
-	}
+	// verify the username/password
+	$pdo = Database::connect();
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$sql = "INSERT INTO customers (name,email,mobile,passwordhash) values(?, ?, ?, ?)";
+	$q = $pdo->prepare($sql);
+	$q->execute(array($name, $email, $mobile, $passwordhash));
+	$data = $q->fetch(PDO::FETCH_ASSOC);
+	Database::disconnect();
+
+
+
+} 
+// if $_POST NOT filled then display login form, below.
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <link   href="css/bootstrap.min.css" rel="stylesheet">
-    <script src="js/bootstrap.min.js"></script>
-</head>
+	<head>
+        <meta charset='UTF-8'>
+        <link href='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css' rel='stylesheet'>
+        <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js'></script>
+	</head>
 
 <body>
     <div class="container">
-
 		<div class="span10 offset1">
-			<div class="row">
-				<h3>Add New</h3>
-			</div>
-	
-			<form class="form-horizontal" action="fr_per_create2.php" method="post" enctype="multipart/form-data">
 
-				<div class="control-group <?php echo !empty(Error)?'error':$nameError'';?>">
-					<label class="control-label">Name</label>
+			<div class="row">
+				<h3>Login</h3>
+			</div>
+
+			<form class="form-horizontal" action="login.php" method="post">
+
+                <div class="control-group">
+					<label class="control-label">Name
 					<div class="controls">
-						<input name="name" type="text"  placeholder="Name" value="<?php echo !empty($name)?$name:'';?>">
-						<?php if (!empty($nameError)): ?>
-							<span class="help-inline"><?php echo $nameError;?></span>
-						<?php endif; ?>
-					</div>
-				</div>
-				
-				
-				<div class="control-group <?php echo !empty($emailError)?'error':'';?>">
-					<label class="control-label">Email</label>
+						<input name="name" type="text" required> 
+					</div>	
+				</div> 
+
+				<div class="control-group">
+					<label class="control-label">Username (Email)</label>
 					<div class="controls">
-						<input name="email" type="text" placeholder="Email Address" value="<?php echo !empty($email)?$email:'';?>">
-						<?php if (!empty($emailError)): ?>
-							<span class="help-inline"><?php echo $emailError;?></span>
-						<?php endif;?>
-					</div>
+						<input name="username" type="text"  placeholder="me@email.com" required> 
+					</div>	
 				</div>
-				
-				<div class="control-group <?php echo !empty($mobileError)?'error':'';?>">
-					<label class="control-label">Mobile Number</label>
+
+                <div class="control-group">
+					<label class="control-label">Number
 					<div class="controls">
-						<input name="mobile" type="text"  placeholder="Mobile Phone Number" value="<?php echo !empty($mobile)?$mobile:'';?>">
-						<?php if (!empty($mobileError)): ?>
-							<span class="help-inline"><?php echo $mobileError;?></span>
-						<?php endif;?>
-					</div>
-				</div>
+						<input name="mobile" type="text" required> 
+					</div>	
+				</div> 
 				
-				<div class="control-group <?php echo !empty($passwordError)?'error':'';?>">
+				<div class="control-group">
 					<label class="control-label">Password</label>
 					<div class="controls">
-						<input id="password" name="password" type="password"  placeholder="password" value="<?php echo !empty($password)?$password:'';?>">
-						<?php if (!empty($passwordError)): ?>
-							<span class="help-inline"><?php echo $passwordError;?></span>
-						<?php endif;?>
-					</div>
-				</div>
-			  
+						<input name="password" type="password" placeholder="not your SVSU password, please" required> 
+					</div>	
+				</div> 
+
 				<div class="form-actions">
-					<button type="submit" class="btn btn-success">Confirm</button>
+					<button type="submit" class="btn btn-success">Sign in</button>
+					&nbsp; &nbsp;
+					<a class="btn btn-primary" href="fr_per_create2.php">Join (New Volunteer)</a>
 				</div>
 				
+				<footer>
+					<small>&copy; Copyright 2019, George Corser and Roman :)
+					</small>
+				</footer>
+				
 			</form>
-			
+
+
 		</div> <!-- end div: class="span10 offset1" -->
 				
     </div> <!-- end div: class="container" -->
+
   </body>
+  
 </html>
