f0a7237b44093ceca5c8f30e03c57dd816f05b4c
Roman <33792116+rklawinski@users.noreply.github.com>
2019-03-16 20:26:19 -0400

    Update customers.php

--- customers.php
+++ customers.php
@@ -1,6 +1,9 @@
 <?php
 
-
+session_start();
+if(!isset($_SESSION["email"])) {
+    header("Location: login.php");
+}
 
 // include the class that handles database connections
 require "../PhpProject/database.php";
